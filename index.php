fd4f9c8d5c177e18d412c51718bd8928a7e89e14
zuzubaidillah <zuzubaidillah@gmail.com>
2024-01-25 07:37:32 +0700

    feat: add cors origin

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+// Mengaktifkan CORS untuk semua domain
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+// Memeriksa metode pra-flighted (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
 require_once __DIR__."/api/Routes.php";

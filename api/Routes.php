87386c4cfe90323dc374347214c7659a4f9bcac3
zuzubaidillah <zuzubaidillah@gmail.com>
2024-04-30 00:54:27 +0700

    book read part 1

--- api/Routes.php
+++ api/Routes.php
@@ -69,6 +69,50 @@ Route::get($base_url . "/api/auth/current", function (){
     $controller->getByToken();
 });
 
+/**
+ * API BOOK
+ * */
+Route::get($base_url . "/api/book", function (){
+    // ambil bearer Token yang di request client
+    $headers = getallheaders();
+    $jwt = null;
+
+    // jika ada array dengan key Authorization
+    if (isset($headers['Authorization'])) {
+        $bearer = explode(' ', $headers['Authorization']);
+        $jwt = $bearer[sizeof($bearer) - 1] ?? null;
+    }
+
+    // jika tidak ditemukan key Authorization
+    if (!$jwt) {
+        http_response_code(401); // Unauthorized
+        echo json_encode(['message' => 'Akses ditolak. Token tidak ditemukan.']);
+        exit();
+    }
+
+    //proses cek token
+    // mengambil user_id yang disimpan didalam token
+    try {
+        $token_jwt = new TokenJwt();
+        $verifikasi_token = $token_jwt->verify($jwt);
+
+        $user = new Users();
+        $result = $user->findId($verifikasi_token['user_id']);
+        if (!$result) {
+            http_response_code(401);
+            echo json_encode(['message' => 'users tidak ditemukan']);
+            exit();
+        }
+    } catch (Exception $e) {
+        http_response_code(401); // Unauthorized
+        echo json_encode(['message' => 'Token tidak valid: ' . $e->getMessage()]);
+        exit();
+    }
+
+    $controller = new BookController();
+    $controller->getBooks();
+});
+
 
 // Add more routes here
 Route::run();
